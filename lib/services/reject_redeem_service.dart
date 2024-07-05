b38799729127ab074a25e0fdec2932c31d5988f0
Parth Deshpande <parthjun27@gmail.com>
2024-07-05 15:34:13 +0530

    CR-4 Added functionality to reject bill with reasons

--- lib/services/reject_redeem_service.dart
+++ lib/services/reject_redeem_service.dart
@@ -30,26 +30,65 @@ class RejectRedeemService {
         'reason': reason, // Add rejection reason
       });
 
-      // Update admin's wallet and ledger
-      QuerySnapshot adminSnapshot = await _firestore
-          .collection('users')
-          .where('role', isEqualTo: 'admin')
-          .get();
-      for (var admin in adminSnapshot.docs) {
-        int adminBalance = admin['wallet']['balance'] ?? 0;
-        int newAdminBalance = adminBalance - coinAmount;
-        await _firestore.collection('users').doc(admin.id).update({
-          'wallet.balance': newAdminBalance,
+      // Begin Firestore transaction
+      await _firestore.runTransaction((transaction) async {
+        // Fetch admin details
+        QuerySnapshot adminSnapshot = await _firestore
+            .collection('users')
+            .where('roll', isEqualTo: 'admin')
+            .get();
+
+        for (var admin in adminSnapshot.docs) {
+          int adminBalance = admin['wallet']['balance'] ?? 0;
+          int newAdminBalance = adminBalance + coinAmount;
+
+          // Update admin balance
+          DocumentReference adminDoc = _firestore.collection('users').doc(admin.id);
+          transaction.update(adminDoc, {
+            'wallet.balance': newAdminBalance,
+          });
+
+          // Ledger entry for admin (adding coins)
+          DocumentReference adminLedgerDocAdd = _firestore.collection('users/${admin.id}/ledger').doc();
+          transaction.set(adminLedgerDocAdd, {
+            'amount': coinAmount,
+            'date': Timestamp.now(),
+            'type': 'redeem',
+            'description': 'Coins temporarily added for rejected redemption from ${userData['email']}',
+          });
+
+          // Subtract coins from admin's balance
+          newAdminBalance = newAdminBalance - coinAmount;
+          transaction.update(adminDoc, {
+            'wallet.balance': newAdminBalance,
+          });
+
+          // Ledger entry for admin (subtracting coins)
+          DocumentReference adminLedgerDocSubtract = _firestore.collection('users/${admin.id}/ledger').doc();
+          transaction.set(adminLedgerDocSubtract, {
+            'amount': -coinAmount,
+            'date': Timestamp.now(),
+            'type': 'redeem',
+            'description': 'Coins removed after rejection of redemption from ${userData['email']}',
+          });
+        }
+
+        // Update user balance
+        int userBalance = userData['wallet']['balance'] ?? 0;
+        int newUserBalance = userBalance + coinAmount;
+        transaction.update(userDoc, {
+          'wallet.balance': newUserBalance,
         });
 
-        // Ledger entry for admin
-        await _firestore.collection('users/${admin.id}/ledger').add({
+        // Ledger entry for user
+        DocumentReference userLedgerDoc = _firestore.collection('users/$userId/ledger').doc();
+        transaction.set(userLedgerDoc, {
           'amount': coinAmount,
           'date': Timestamp.now(),
           'type': 'redeem',
-          'description': 'Coins received from ${userData['email']}',
+          'description': 'Coins refunded due to rejection of redemption request',
         });
-      }
+      });
     } else {
       throw Exception("User not found");
     }
